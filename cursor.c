c4d6a92a711f2be54c2c18e57ae1f28d4c6e57bc
lemonsugar <nikhiljaiswalg2013@gmail.com>
2026-03-31 01:17:41 +0530

    .

---
+++ cursor.c
@@ -0,0 +1,52 @@
+#include<conio.h>
+#include<stdio.h>
+
+
+void q1(int a,int b){		     //we take 1(\n)=2*(" ")
+int i,j,k,x,y;
+for(k=1;2*a>=k;k++){
+printf(" ");               //it seems th esize of space and /n not similar
+}
+printf("@");
+for(j=1;b>=j;j++){                                       //given in tp1
+printf("\n");
+}
+}
+
+
+void main(){
+int i,j=0,k=25,x,y,s=0;
+char ch,t,sp = 'p';
+q1(0,0);
+for(;ch!=sp;t=t){
+if(kbhit()){
+   ch=getch();
+   if(ch == 'o'){
+     q1(s,j+1); //for something crazy change coordinates
+     j=j+1;
+
+     }
+   if(ch == 'k'&& j>=1){
+     q1(s,j-1); //same here
+     if(j>0){
+     j=j-1;
+     }
+     }
+   if(ch == 'l'){
+     q1(s+1,j);
+     s=s+1;
+     }
+   if(ch == 'j'&& s>=1){
+     q1(s-1,j);
+     s=s-1;
+     }
+}
+}
+
+
+/*
+printf("%d,",getch());
+*/
+getch();
+
+}
